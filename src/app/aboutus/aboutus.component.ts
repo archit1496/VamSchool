65c21f8ae94e53ba4d8a562848d851196de62b13
alekyaragipally <35074678+alekyaragipally@users.noreply.github.com>
2020-10-18 14:18:50 +0530

    Home screen scroll

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutus',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
+
+  @Input('fromHome') fromHome: boolean;
 
   constructor() { }
 
--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contactus',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
+
+  @Input('fromHome') fromHome: boolean;
 
   constructor() { }
 
