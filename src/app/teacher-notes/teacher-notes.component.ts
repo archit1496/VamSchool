e21fa75ea314b6796d9529a2dc1f40fdfe72a628
43215-Anuj <43952928+43215-Anuj@users.noreply.github.com>
2021-02-12 22:07:33 +0530

    study material test

--- src/app/teacher-notes/teacher-notes.component.ts
+++ src/app/teacher-notes/teacher-notes.component.ts
@@ -28,6 +28,9 @@ export class TeacherNotesComponent implements OnInit {
   newtopic;
   url = '';
   notesOrVideo: string;
+  filesValue;
+  isDatainTopicWise: boolean = false;
+  
   constructor(public teacherService:TeacherService,private toaster: ToastrService) { }
   get Math() {
     return Math;
@@ -35,12 +38,19 @@ export class TeacherNotesComponent implements OnInit {
     get Infinity() {
     return Infinity;
   }
+
   ngOnInit() {
     this.fetchNotesDataSubject();
     this.fetchTeacher();
   }
 
+  submitForm(data){
+    // debugger;
+    console.log(data);
+  }
+
   postData(){
+    console.log("Save clicked")
     const data: FormData = new FormData();
     data.append('name', this.topic);
     data.append('course', this.selectedData.id.toString());
@@ -59,26 +69,33 @@ export class TeacherNotesComponent implements OnInit {
   }
 
   fetchNotesDataSubject() {
-
     // this.isLoading = true;
     this.teacherService.fetchTeacherCourse().subscribe(res => {
       // this.isLoading = false;
       this.studentNotesDataSubjectWise = res.data;
+      this.isDatainTopicWise = true;
     });
   }
+
   fetchNotesDataTopicWise(id:number) {
     // this.isLoading = true;
     this.teacherService.fetchNoteData(id).subscribe(res => {
       // this.isLoading = false;
       this.studentNotesDataSubjectWise=[];
       this.studentNotesDataTopicWise = res;
+      if(this.studentNotesDataTopicWise.length === 0 ) {
+        this.isDatainTopicWise = false;
+      } else {
+        this.isDatainTopicWise = true;
+      }
     });
-   
   }
+
   fetchNotesData() {
-this.studentNotesDataTopicWise = [];
- this.notesVideos = true;
+    this.studentNotesDataTopicWise = [];
+    this.notesVideos = true;
   }
+
   getDate(date){
     return (new Date(date).getDate()+'-'+new Date(date).getMonth()+'-'+new Date(date).getFullYear());
   }
@@ -86,6 +103,7 @@ this.studentNotesDataTopicWise = [];
   onNotesClick(url){
     window.open(url);
   }
+
   fetchTeacher() {
     this.teacherService.fetchTeacher().subscribe(res => {
       this.teacherId = res.id;
@@ -106,7 +124,7 @@ this.studentNotesDataTopicWise = [];
   }
 
   openForm2() {
-this.topic = '';
+    this.topic = '';
     document.getElementById('myForm2').style.display = 'block';
     // this.topicName = '';
     // this.topicQuestion = '';
@@ -114,7 +132,6 @@ this.topic = '';
     //  (<HTMLInputElement>document.getElementById("uploadCaptureInputFile")).value = "";
     const someElement = document.getElementById('mainOuterDiv');
     someElement.className += ' newclass';
-
   }
 
   fetchCourse() {
@@ -123,40 +140,31 @@ this.topic = '';
     });
   }
 
-
   closeForm() {
-
     document.getElementById('myForm').style.display = 'none';
     const element = document.getElementById('mainOuterDiv');
     element.classList.remove('newclass');
-
   }
 
   closeForm2() {
-
     document.getElementById('myForm2').style.display = 'none';
     const element = document.getElementById('mainOuterDiv');
     element.classList.remove('newclass');
-
   }
 
   uploadNotes(fileInput) {
     this.files = fileInput.target.files;
+    this.filesValue = fileInput.target.value;
   }
 
   postNotesData(){
-
     this.spinnerFlag = true;
     const formData: FormData = new FormData();
-if (this.url === 'notes') {
-  formData.append('note', this.files[0], this.files[0].name);
-} else {
-  formData.append('video', this.files[0], this.files[0].name);
-}
-  
-
-    
-
+    if (this.url === 'notes') {
+      formData.append('note', this.files[0], this.files[0].name);
+    } else {
+      formData.append('video', this.files[0], this.files[0].name);
+    }
 
     formData.append('name', this.newtopic);
     formData.append('dir', this.selectedNotesData.id);
@@ -173,18 +181,45 @@ if (this.url === 'notes') {
     });
   }
 
-
   fetchNotes(type){
     if (type === 'note') {
       this.url = 'notes';
-      this.notesOrVideo = 'Notes'
+      this.notesOrVideo = 'Notes';
+
     } else if (type === 'vid') {
       this.url = 'videos';
       this.notesOrVideo = 'Videos'
     }
-      this.teacherService.getTeacherNotes(this.selectedNotesData.id, this.url).subscribe(res => {
-      this.notesVideos = false;
-      this.notesData = res.notes_obj;
-    })
+
+    this.teacherService.getTeacherNotes(this.selectedNotesData.id, this.url).subscribe(res => {
+    this.notesVideos = false;
+    this.notesData = res.notes_obj;
+    }) 
+  }
+
+  // validation of input file size and extension
+  Validate(folder_filetype) {
+    if(this.filesValue!='') {
+      var checkimg = this.filesValue.toLowerCase();
+      if( folder_filetype == 'Notes') {
+        if (!checkimg.match(/(\.jpg|\.png|\.JPG|\.PNG|\.jpeg|\.pdf)$/)) { 
+          alert("File format not supported"); 
+          return false;
+        }
+      }
+      else if( folder_filetype == 'Videos') {
+        if (!checkimg.match(/(\.mp4|\.flv|\.mkv)$/)) { 
+          alert("File format not supported"); 
+          return false;
+        }
+      }
+    }
+
+    if(this.files[0].size > 5120) {
+        alert("File size too large");
+        return false;
+    }
+    this.postNotesData();
+    return true;
   }
 }
