c2e54b2bdb4a80af5be1fd5068e55431f37195d7
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-02-22 23:20:32 +0530

    env file changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // appUrl: 'https://api.onwardlearn.in/'
+  appUrl: 'https://api.onwardlearn.in/'
   // appUrl: 'http://localhost:8000/'
-  appUrl: 'https://api.vamschool.in/'
+  // appUrl: 'https://api.vamschool.in/'
 };
